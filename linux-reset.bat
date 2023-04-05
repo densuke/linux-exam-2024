817bff3c1a2472ecc1c472d7f74394ea1eae9dcb
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2023-04-06 08:38:21 +0900

    [fix][win] docker-desktop-data対策
    理由はよくわからないけど時々
    WSLの起動先がdocker-desktop-dataになってしまうので
    実行前に切り替えて対応するようにしてみた。

--- linux-reset.bat
+++ linux-reset.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d %~dp0
+wsl --set-default docker-desktop > nul
 wsl bash ./linux-reset %%1
 
--- linux-setup.bat
+++ linux-setup.bat
@@ -1,3 +1,4 @@
 @echo off
 cd /d %~dp0
+wsl --set-default docker-desktop > nul
 wsl bash ./linux-setup
